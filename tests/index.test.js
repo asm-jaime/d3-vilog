2706bdb1adb81a70bf0891f3ebe1bfbee8a92ac1
asm-jaime <asm.jaime@gmail.com>
2017-11-22 15:23:50 +1000

    add line chart and test it

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,13 +6,13 @@
 import { logger } from '../index.js';
 
 describe('graph', function() {
-  it.skip('clear grpaph: ', function() {//{{{
+  it.skip('clear grpaph: ', function() { //{{{
     const data = {
       'nodes': [
-        { 'id': '1'},
-        { 'id': '2'},
-        { 'id': '3'},
-        { 'id': '4'},
+        { 'id': '1' },
+        { 'id': '2' },
+        { 'id': '3' },
+        { 'id': '4' },
       ],
       'links': [
         { 'source': '1', 'target': '2', 'width': 10, 'length': 100 },
@@ -20,17 +20,17 @@ describe('graph', function() {
         { 'source': '1', 'target': '4', 'width': 15, 'length': 150 },
       ]
     };
-    logger({data, dest: './tests/clear_graph', type: 'graph' });
-  });//}}}
-  it.skip('full cirled grpaph: ', function() {//{{{
+    logger({ data, dest: './tests/clear_graph', type: 'graph' });
+  }); //}}}
+  it.skip('full cirled grpaph: ', function() { //{{{
     const data = {
       'nodes': [
-        { 'id': '1','radius': 5, 'group': 2},
-        { 'id': '2','radius': 10, 'group': 2},
-        { 'id': '3','radius': 10, 'group': 4},
-        { 'id': '4','radius': 10, 'group': 4},
-        { 'id': '5','radius': 15, 'group': 4},
-        { 'id': '6','radius': 15, 'group': 4},
+        { 'id': '1', 'radius': 5, 'group': 2 },
+        { 'id': '2', 'radius': 10, 'group': 2 },
+        { 'id': '3', 'radius': 10, 'group': 4 },
+        { 'id': '4', 'radius': 10, 'group': 4 },
+        { 'id': '5', 'radius': 15, 'group': 4 },
+        { 'id': '6', 'radius': 15, 'group': 4 },
       ],
       'links': [
         { 'source': '1', 'target': '2', 'width': 10, 'length': 100 },
@@ -40,9 +40,9 @@ describe('graph', function() {
         { 'source': '4', 'target': '6', 'width': 10, 'length': 250 },
       ]
     };
-    logger({data, dest: './tests/full_circle_graph', type: 'graph' });
-  });//}}}
-  it('pictured graph: ', function() {//{{{
+    logger({ data, dest: './tests/full_circle_graph', type: 'graph' });
+  }); //}}}
+  it.skip('pictured graph: ', function() { //{{{
     const data = {
       'nodes': [
         { 'id': '1', 'href': 'https://raw.githubusercontent.com/asm-jaime/yoba-package/master/pic.size(128x128)/zsb-g_story.png' },
@@ -67,20 +67,54 @@ describe('graph', function() {
         { 'source': '9', 'target': '8', 'width': 1, 'length': 300 },
       ]
     };
-    logger({data, dest: './tests/pictured_graph', type: 'graph' });
-  });//}}}
-  it.skip('big graph: ', function() {//{{{
+    logger({ data, dest: './tests/pictured_graph', type: 'graph' });
+  }); //}}}
+  it.skip('big graph: ', function() { //{{{
     const nodes = [];
     const links = [];
-    const node = { 'id': '0','radius': 5, 'group': 2};
+    const node = { 'id': '0', 'radius': 5, 'group': 2 };
     nodes.push(node);
-    for(let i = 1; i < 1000; ++i){
-      const node = { 'id': i.toString(),'radius': 5, 'group': 2};
-      const link = { 'source': (i-1).toString(), 'target': node.id, 'width': 10, 'length': 100 };
+    for (let i = 1; i < 1000; ++i) {
+      const node = { 'id': i.toString(), 'radius': 5, 'group': 2 };
+      const link = { 'source': (i - 1).toString(), 'target': node.id, 'width': 10, 'length': 100 };
       nodes.push(node);
       links.push(link);
     }
-    const data = {nodes, links};
-    logger({data, dest: './tests/big_graph', type: 'graph' });
+    const data = { nodes, links };
+    logger({ data, dest: './tests/big_graph', type: 'graph' });
+  }); //}}}
+});
+
+describe('line', function() {
+  it('line: ', function() {//{{{
+    function generateData() {
+      const fst = [],
+        snd = [],
+        trd = [],
+        fth = [],
+        ffth = [],
+        r1 = Math.random(),
+        r2 = Math.random(),
+        r3 = Math.random(),
+        r4 = Math.random(),
+        r5 = Math.random();
+
+      for (var i = 0; i < 100; i++) {
+        fst.push([i, r1 * Math.sin(r2 + i / (10 * (r4 + .5)))]);
+        snd.push([i, r2 * Math.cos(r3 + i / (10 * (r3 + .5)))]);
+        trd.push([i, r3 * Math.sin(r1 + i / (10 * (r2 + .5)))]);
+        fth.push([i, r4 * Math.cos(r4 + i / (10 * (r1 + .5)))]);
+        ffth.push([i, (r1+r2+r3+r4+r5)/5]);
+      }
+      return [
+        { data: fst, label: 'first sin1' },
+        { data: snd, label: 'second cos1' },
+        { data: trd, label: 'third sin2' },
+        { data: fth, label: 'fourth cos2' },
+        { data: ffth, label: 'fifth line' }
+      ];
+    }
+
+    logger({ data: generateData(), dest: './tests/clear_line', type: 'line' });
   });//}}}
 });
